d4d4c9383254ae549629701c4d4b9b1d50fa9c2e
Bright Chang <75425626@qq.com>
2018-05-09 14:17:32 +0800

    Add files via upload

---
+++ Document Classification/Chinese document classification/Clean the data.py
@@ -0,0 +1,7 @@
+# Date: 2018 - 05 - 08
+# Author: Haoliang Chang
+# Data: The data could be found in here：http://thuctc.thunlp.org/
+
+import numpy as np
+
+def raw_to_words(raw, remove_stopwords = False):
