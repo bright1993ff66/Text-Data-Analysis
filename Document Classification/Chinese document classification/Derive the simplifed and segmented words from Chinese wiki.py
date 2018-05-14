7440c5280f0fc47c54223047b52948d076ab274f
Bright Chang <75425626@qq.com>
2018-05-14 17:26:26 +0800

    Update Derive the simplifed and segmented words from Chinese wiki.py

--- Document Classification/Chinese document classification/Derive the simplifed and segmented words from Chinese wiki.py
+++ Document Classification/Chinese document classification/Derive the simplifed and segmented words from Chinese wiki.py
@@ -1,11 +1,9 @@
 """
 Date: 2018 - 05 - 13
-Codes and more specific guidance could be found in this site: 
-https://www.jianshu.com/p/ec27062bd453
-The zh_wiki and langconv modules, which are used for transforming 
-the complex Chinese to the simplified Chinese, could be found in
+1. Codes and more specific guidance could be found in this site: https://www.jianshu.com/p/ec27062bd453
+2. The zh_wiki and langconv modules, which are used for transforming the complex Chinese to the simplified Chinese, could be found in
 here: https://github.com/skydark/nstools/tree/master/zhtools
-Data: The data could be found in here：http://thuctc.thunlp.org/
+3. Data: The data could be found in here：http://thuctc.thunlp.org/
 """
 
 # Import the modules
@@ -22,8 +20,8 @@ import jieba.posseg as pseg
 from gensim.corpora import WikiCorpus
 
 if __name__ == '__main__':
-	# Export all the Chinese words in the xml file into a txt file
-    program = 'F:\Data Analysis\github\THUCNews\Codes\Get Chinese word vectors from wiki.py'
+    # Export all the Chinese words in the xml file into a txt file
+    program = 'Derive the simplifed and segmented words from Chinese wiki.py'
     logger = logging.getLogger(program)
  
     logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s')
@@ -34,8 +32,8 @@ if __name__ == '__main__':
         print(globals()['__doc__'] % locals())
         sys.exit(1)
         
-    data_file_path = 'F:\Data Analysis\github\THUCNews\data\zhwiki-latest-pages-articles.xml.bz2'
-	output_path = 'F:\Data Analysis\github\THUCNews\data\zhwiki.txt'
+    data_file_path = 'zhwiki-latest-pages-articles.xml.bz2'
+	output_path = 'zhwiki.txt'
 
     inp, outp = data_file_path, output_path
     space = " "
@@ -54,40 +52,40 @@ if __name__ == '__main__':
     output.close()
     logger.info("Finished Saved "+str(i)+" articles.")
 
-	# Transform the complex Chinese to the simplified Chinese
-	file_to_be_processed = 'F:\Data Analysis\github\THUCNews\data\zhwiki.txt'
-	output_file_path = 'F:\Data Analysis\github\THUCNews\data\zhwiki_simple.txt'
-	output = open(output_file_path, 'w', encoding = 'UTF-8')
+    # Transform the complex Chinese to the simplified Chinese
+    file_to_be_processed = 'zhwiki.txt'
+    output_file_path = 'zhwiki_simple.txt'
+    output = open(output_file_path, 'w', encoding = 'UTF-8')
 
-	i = 0
-	
-	with open(file_to_be_processed, mode = 'r', encoding = 'UTF-8') as f:
-	    for line in f:
-	        output.write(cht_to_chs(line))
-	        i = i+1
-	        if (i % 10000 == 0):
-	            logger.info("Saved "+str(i)+" articles.")
-	        
-	    output.close()
-		logger.info("Finished Saved "+str(i)+" articles.")
-	
-	# Segment the words using jieba
-	file_to_be_stemmed = open('F:\Data Analysis\github\THUCNews\data\zhwiki_simple.txt', mode = 'r', encoding = 'UTF-8')
-	output_file = open('F:\Data Analysis\github\THUCNews\data\zhwiki_simple.txt', mode = 'w', encoding = 'UTF-8')
-	print('The file is opened~')
+    i = 0
+    
+    with open(file_to_be_processed, mode = 'r', encoding = 'UTF-8') as f:
+        for line in f:
+            output.write(cht_to_chs(line))
+            i = i+1
+            if (i % 10000 == 0):
+                logger.info("Saved "+str(i)+" articles.")
+            
+        output.close()
+    	logger.info("Finished Saved "+str(i)+" articles.")
+    
+    # Segment the words using jieba
+    file_to_be_stemmed = open('zhwiki_simple.txt', mode = 'r', encoding = 'UTF-8')
+    output_file = open('zhwiki_simple_segmented.txt', mode = 'w', encoding = 'UTF-8')
+    print('The file is opened~')
 
-	lineNum = 1
-	line = file_to_be_stemmed.readline()
+    lineNum = 1
+    line = file_to_be_stemmed.readline()
 
-	while line:
-	    print('Line(article) ', lineNum, 'is processing')
-	    segmentation = jieba.cut(line, cut_all = False)
-	    line_seg = ' '.join(segmentation)
-	    output_file.writelines(line_seg)
-	    lineNum += 1
-	    line = file_to_be_stemmed.readline()
-	    
-	print('finished')
-	file_to_be_stemmed.close()
-	output_file.close()
-		
+    while line:
+        print('Line(article) ', lineNum, 'is processing')
+        segmentation = jieba.cut(line, cut_all = False)
+        line_seg = ' '.join(segmentation)
+        output_file.writelines(line_seg)
+        lineNum += 1
+        line = file_to_be_stemmed.readline()
+        
+    print('finished')
+    file_to_be_stemmed.close()
+    output_file.close()
+    	
